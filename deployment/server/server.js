8dd16e0b4235fe30527624bc1e07b23949181bcf
Keaton Player <keatonplayer18@gmail.com>
2022-04-27 17:32:58 -0600

    Added stuff

--- deployment/server/server.js
+++ deployment/server/server.js
@@ -1,9 +1,7 @@
 const express = require ('express')
 const path = require('path')
 const app = express()
-app.get('/', function (req, res){
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+
 app.get('/css', function (req, res){
     res.sendFile(path.join(__dirname, '../public/style.css'))
 })
